ce49b047296bd8a5af5577ea2a114230dd285ff6
daniidoblass <danieldoblasflorido.sonjose@fundacionloyola.net>
2022-12-12 10:33:20 +0100

    voooooo

--- src/fpproject/hello.java
+++ src/fpproject/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ffjjjj");
+		System.out.println("ffjj");
 		System.out.println("FCDSAFSE");
 	}
 
